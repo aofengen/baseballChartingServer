6af9b73421c25a32fc7634647803a3fd5975e02c
Aaron Ofengender <aofengen@gmail.com>
2017-10-20 11:40:32 -0400

    editing db routes

--- controllers/hstats.js
+++ controllers/hstats.js
@@ -2,25 +2,39 @@ const sequelize = require('../db.js');
 const hStats = require('../models/hstats.js');
 
 exports.addhStats = function(req,res) {
-    let stats = {
-        team: req.body.team,
-        player: req.body.player,
-        atbats: req.body.abs,
-        singles: req.body.singles,
-        doubles: req.body.doubles,
-        triples: req.body.triples,
-        homeruns: req.body.hrs,
-        strikeouts: req.body.strikeouts,
-        walks: req.body.walks,
-        hitbypitches: req.body.hbp,
-        sacflies: req.body.sf,
+    let team = req.body.team;
+    let player = req.body.player;
+    let atbats = req.body.abs;
+    let singles = req.body.singles;
+    let doubles = req.body.doubles;
+    let triples = req.body.triples;
+    let homeruns = req.body.hrs;
+    let strikeouts = req.body.strikeouts;
+    let walks = req.body.walks;
+    let hitbypitches = req.body.hbp;
+    let sacflies = req.body.sf;
+    let rbis = req.body.rbis;
+    let runs = req.body.runs;
+    let stolenbases = req.body.sb;
+    let caughtstealing =  req.body.cs;
+
+    hStats.create({
+        team: team,
+        player: player,
+        atbats: atbats,
+        singles: singles,
+        doubles: doubles,
+        triples: triples,
+        homeruns: homeruns,
+        strikeouts: strikeouts,
+        walks: walks,
+        hitbypitches: hitbypitches,
+        sacflies: req.body.sacflies,
         rbis: req.body.rbis,
         runs: req.body.runs,
-        stolenbases: req.body.sb,
-        caughtstealing: req.body.cs
-    }
-
-    hStats.create(stats)
+        stolenbases: req.body.stolenbases,
+        caughtstealing: req.body.caughtstealing
+    })
     .then(
         function createSuccess(hstats) {
             res.json({
